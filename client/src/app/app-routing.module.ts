88399ed6ffb09e3765be57e6967b0cf09d1efdfb
ruchiniwalkar01 <niwalkar.r@northeastern.edu>
2020-08-19 18:37:30 -0400

    added all components

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SalesComponent } from './sales/sales.component';
+import { SalesDetailsComponent } from './sales-details/sales-details.component';
+import { AddSalesComponent } from './add-sales/add-sales.component';
+import { EditSalesComponent } from './edit-sales/edit-sales.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: SalesComponent,
+    path: 'sales',
+    data: { title: 'List of Sales' },
+  },
+  {
+    component: SalesDetailsComponent,
+    path: 'sales-details/:id',
+    data: { title: 'Sales Details' },
+  },
+  {
+    component: AddSalesComponent,
+    path: 'add-sales',
+    data: { title: 'Add Sales' },
+  },
+  {
+    component: EditSalesComponent,
+    path: 'edit-sales/:id',
+    data: { title: 'Edit Sales' },
+  },
+
+  { path: '', redirectTo: 'sales', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
